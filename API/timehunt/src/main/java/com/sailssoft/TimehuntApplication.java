5ddfcad92b6d672be982d58f532295aa95ebb373
vvrkFSD <ramakrishna.vasamsetti@sailssoftware.com>
2022-01-17 09:49:29 +0530

    Initial Commit

---
+++ API/timehunt/src/main/java/com/sailssoft/TimehuntApplication.java
@@ -0,0 +1,13 @@
+package com.sailssoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimehuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimehuntApplication.class, args);
+	}
+
+}
