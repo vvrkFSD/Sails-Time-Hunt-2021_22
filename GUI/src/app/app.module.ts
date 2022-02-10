eafc33d81526a04282e5ba48d612ede9bca6f471
gowrikamisetti <gowrikamisetti@gmail.com>
2022-02-10 07:35:05 +0530

    changes made in Login page

--- GUI/src/app/app.module.ts
+++ GUI/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './common-components/login/login.component';
 import { ForgotPasswordComponent } from './common-components/forgot-password/forgot-password.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ForgotPasswordComponent
-  ],
+  declarations: [AppComponent, LoginComponent, ForgotPasswordComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
